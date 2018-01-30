3b1e0f531d9b786b789bea3a878e18c44d5a402d
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-30 12:59:56 +0100

    added build script

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,10 @@ var config = {
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor', 'manifest']
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
     ]
 }
 module.exports = config;
